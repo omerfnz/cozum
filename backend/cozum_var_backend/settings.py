4bfe235584f26e8eca492ef6fae506557ce4ee1a
omerfnz <76820647+omerfnz@users.noreply.github.com>
2025-08-23 18:19:24 +0300

    asd

--- backend/cozum_var_backend/settings.py
+++ backend/cozum_var_backend/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-+hvznaue8j7s$d#(n!fij
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1,3.126.55.85').split(',')
 
 
 # Application definition
