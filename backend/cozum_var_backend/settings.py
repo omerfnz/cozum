4b0fbe66c529d23504043bfb3abb293d595fc5fb
omerfnz <76820647+omerfnz@users.noreply.github.com>
2025-08-23 20:12:50 +0300

    volume

--- backend/cozum_var_backend/settings.py
+++ backend/cozum_var_backend/settings.py
@@ -168,7 +168,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
