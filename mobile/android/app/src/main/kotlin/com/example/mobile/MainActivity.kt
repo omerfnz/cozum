52019f96dbd4dd087bffde4a5dbc7fa46f25cd6b
omerfnz <76820647+omerfnz@users.noreply.github.com>
2025-08-28 00:35:26 +0300

    lasteditndkjava

--- mobile/android/app/src/main/kotlin/com/example/mobile/MainActivity.kt
+++ mobile/android/app/src/main/kotlin/com/example/mobile/MainActivity.kt
@@ -1,15 +1,5 @@
 package com.example.mobile
 
-import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        // Xiaomi MIUI için splash screen düzeltmesi
-        super.onCreate(savedInstanceState)
-        
-        // MIUI cihazlarda splash screen görünürlüğünü artırmak için
-        window.statusBarColor = resources.getColor(android.R.color.transparent, theme)
-        window.navigationBarColor = resources.getColor(android.R.color.transparent, theme)
-    }
-}
+class MainActivity : FlutterActivity()
