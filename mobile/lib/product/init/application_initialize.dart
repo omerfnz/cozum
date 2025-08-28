c2613a1183aa69bfc459c9fae1b2f086054255f1
omerfnz <76820647+omerfnz@users.noreply.github.com>
2025-08-28 16:49:14 +0300

    splashnative

--- mobile/lib/product/init/application_initialize.dart
+++ mobile/lib/product/init/application_initialize.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:get_it/get_it.dart';
 import 'package:logger/logger.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 import 'service_locator.dart';
 import '../service/error/global_error_handler.dart';
@@ -10,16 +11,21 @@ import '../service/error/global_error_handler.dart';
 final class ApplicationInitialize {
   /// Initialize the application
   Future<void> make() async {
-    WidgetsFlutterBinding.ensureInitialized();
-    
+    // Splash ekranını, başlatma tamamlanana kadar koru
+    final binding = WidgetsFlutterBinding.ensureInitialized();
+    FlutterNativeSplash.preserve(widgetsBinding: binding);
+
     // Initialize EasyLocalization
     await EasyLocalization.ensureInitialized();
-    
+
     // Setup dependency injection
     await setupServiceLocator();
-    
+
     // Setup global error handling
     final logger = GetIt.I<Logger>();
     GlobalErrorHandler.instance.initialize(logger);
+
+    // Başlatma işlemleri tamamlandı, splash ekranını kaldır
+    FlutterNativeSplash.remove();
   }
 }
